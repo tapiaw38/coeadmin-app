792a1540b86d94292bc1ca014260a47a81bf57d7
walter <walter.tapia@bigbox.com.ar>
2022-01-13 00:25:01 -0300

    change the domine name

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,7 +5,7 @@ from .base import env
 
 # Base
 SECRET_KEY = env('DJANGO_SECRET_KEY')
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['tratohecho.net.ar','www.tratohecho.net.ar'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['agrapi.com.ar','www.agrapi.com.ar'])
 
 # Databases
 DATABASES['default'] = env.db('DATABASE_URL')  # NOQA
