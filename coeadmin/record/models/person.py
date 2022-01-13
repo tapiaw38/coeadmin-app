bccdf84df686d742e0c1572270034b4e1f131d06
walter <walter.tapia@bigbox.com.ar>
2022-01-13 11:55:47 -0300

    change field required

--- coeadmin/record/models/person.py
+++ coeadmin/record/models/person.py
@@ -29,7 +29,6 @@ class Person(BaseModel):
     date_birth = models.DateField(
         null=True,
         blank=True,
-        default=01/01/2000
     )
     
     gender = models.CharField(max_length=10)
